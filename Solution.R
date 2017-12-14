d3de64845d96bce86d84e80b5421c9a17dd45c45
Corrnado <luasa@aol.com>
2017-12-14 16:52:45 -0500

    Get the time frame of the data set

--- Solution.R
+++ Solution.R
@@ -10,11 +10,16 @@ test <- as.tibble(fread('test.csv'))
 summary(train)
 glimpse(train)
 
-## vendor_is is only 1 and 2, supposely it is the id number for different taxi companies.
-## pickup_datetime and dropoff_datetime need to be reformed to be used in the future.
-## passenger_count is a integer between 1 and 9.
-## store_and_fwd_flag is a Y and N boolean variable indicates weather the trip record was 
+## Vendor_is is only 1 and 2, supposely it is the id number for different taxi companies.
+## Pickup_datetime and dropoff_datetime need to be reformed to be used in the future.
+## Passenger_count is a integer between 1 and 9.
+## Store_and_fwd_flag is a Y and N boolean variable indicates weather the trip record was 
 ## held in vehicle memory before sending to the vendor because of the connectivity. it 
 ## could have some correlation with the destination.
-## trip_duration is our target variable to predict.
+## Trip_duration is our target variable to predict. 
 
+# Get the time boundary of the data set.
+pdate = ymd_hms(train$pickup_datetime)
+summary(pdate)
+
+# The time frame of the data collection is 01.01.2016 to 06.30.2016.
